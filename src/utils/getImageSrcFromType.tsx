ce5db870b123ef94ea5656fe513f66915a5b174c
MateoValenciaGaviria <mateo.valencia.g@gmail.com>
2021-06-08 23:19:19 -0500

    Base Url added to getImageSrc method

--- src/utils/getImageSrcFromType.tsx
+++ src/utils/getImageSrcFromType.tsx
@@ -1,4 +1,4 @@
-const baseExtension = '/images/file_extensions/'
+const baseExtension = `${process.env.PUBLIC_URL}/images/file_extensions/`;
 
 export const getImageSrcFromType = (type: string = '', color?:number) => {
     return `${baseExtension}${type}${color || 'default'}.png`;
